840506a2c38baed2be4d6741ef813c235314edb2
patr45195 <patr45195@gmail.com>
2024-07-20 14:22:05 +0300

    +

--- src/App.js
+++ src/App.js
@@ -1,6 +1,14 @@
 import { memo, useState } from "react";
 import "./App.css";
 
+const chainsOption = [
+  { id: 1, name: "chain1", comment: "com1" },
+  { id: 2, name: "chain2", comment: "com2" },
+  { id: 3, name: "chain3", comment: "com3" },
+  { id: 4, name: "chain4", comment: "com4" },
+  { id: 5, name: "chain5", comment: "com5" },
+];
+
 const Chain = memo(({ item }) => {
   return <div className="chain_item">{`Item №${item}`}</div>;
 });
