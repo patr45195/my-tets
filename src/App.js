2c5086a56c1f41ed6afbc6cff0f2444a38c3c753
patr45195 <patr45195@gmail.com>
2024-07-20 14:16:10 +0300

    +

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
 import { useState } from "react";
 import "./App.css";
 
+const Chain = ({ item }) => {
+  return <div className="chain_item">{`Item №${item}`}</div>;
+};
+
 function App() {
-  const [chains, setChains] = useState([1, 2]);
+  const [chains, setChains] = useState([1, 2, 3, 4, 5]);
+
+  const addChain = () => {
+    setChains((prev) => [...prev, chains.length + 1]);
+  };
 
   return (
     <div>
       {chains.map((item, index) => (
-        <div key={index}>{item}</div>
+        <Chain key={index} item={item} />
       ))}
+      <button onClick={addChain}>Add chain</button>
     </div>
   );
 }
